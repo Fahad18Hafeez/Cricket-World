72a07d509d789443e6f86c880513e01fa061f561
Fahad18Hafeez <fahadhafeez2014@gmail.com>
2026-07-20 21:20:53 +0500

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,120 @@
+// console.log("My name is Fahad Hafeez");
+// console.log("I am learning Java Script");
+// console.log("Welcome to coding");
+// console.log("HTML+CSS Completed");
+// console.log("First Day of java script learning");
+// let name = "Fahad Hafeez";
+// console.log(name);
+// let name = "Muhammad Fahad Hafeez";
+// let age = 19;
+// let country = "Pakistan";
+// let isStudent = true;
+// let hobby = "Coding";
+
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof country);
+// console.log(typeof isStudent);
+// console.log(typeof hobby);
+// let a = 15;
+// let b = 4;
+
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** 2);
+// alert("Welcome to JavaScript!");
+// let name = prompt("Enter your Name");
+// console.log(name);
+// let name=prompt("your name");
+// alert("welcome"+name);
+// let age = Number(prompt("Enter Your Age"));
+// console.log("Age: " + age);
+// let a=5;
+// console.log(a!=5);
+// let password="1234";
+// console.log(password==="1234"
+// )
+// console.log(!true);
+// console.log(!false);
+// let age=15;
+// if(age>=28){
+// console.log("you can vote");}
+// else if(age<=28)
+// {console.log("You can it");
+//    }
+//    else{
+//     console.log("wrong");
+//    }
+// let day=Number(prompt("enter day number"));
+// switch(day){
+//    case 1:
+//       console.log("Monday");
+//       break;
+//       case 2:
+//          console.log("tuesday");
+//          break;
+//          case 3:
+//             console.log("Wednesday");
+//             break;
+// default:
+// console.log("invalid day");}
+// for(let i=1; i<=5; i++){
+//    console.log(i);
+// }
+// let i=1
+// while(i<=5)
+// {console.log(i);
+// i++;
+// }
+// let i=10;
+// while(i>=1){
+//     console.log(i);
+//     i--;
+// }
+// let i=1;
+// do{
+//     console.log(i);
+//     i++;
+// }
+// while(i<=5);
+// function greet(){
+//     console.log("welcome");
+// }
+// greet();
+// greet();
+// function add(a, b) {
+//     return a + b;
+// }
+
+// let result = add(5, 6);
+// console.log(result);
+// function greet(a,b){
+//     console.log(a+b);
+// }
+// greet(3,4);
+// greet(5,6);
+// greet(4,8);
+// let x = 100;
+
+// function show() {
+//     console.log(x);
+// }
+
+// show();
+// function test() {
+//     let y = 50;
+//     console.log(y);
+// }
+
+// test();
+
+// console.log(y);
+// const subtract=(a, b) =>
+//      a - b;
+// console.log(subtract(20, 5));
+let fruits=["apple","bnana","mango","Orange"];
+console.log(fruits[0]);
+console.log(fruits[3]);
